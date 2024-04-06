2d6571a98244cf4c60f64cc7d8db89c02bd66067
Avani <avaniprajapati2810@gmail.com>
2024-04-06 10:18:51 +0530

    Netflix clone

---
+++ netflix.js
@@ -0,0 +1,21 @@
+let sixth1=document.querySelector(".sixth1");
+let sicon2=document.querySelector(".sicon2");
+let sixth2=document.querySelector(".sixth2");
+let sicon3=document.querySelector(".sicon3");
+let sixth3=document.querySelector(".sixth3");
+let sicon4=document.querySelector(".sicon4");
+let sixth4=document.querySelector(".sixth4");
+let sicon5=document.querySelector(".sicon5");
+
+sixth1.addEventListener("click",()=>{
+  sicon2.classList.toggle("display")
+})
+sixth2.addEventListener("click",()=>{
+  sicon3.classList.toggle("display")
+})
+sixth3.addEventListener("click",()=>{
+  sicon4.classList.toggle("display")
+})
+sixth4.addEventListener("click",()=>{
+  sicon5.classList.toggle("display")
+})
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Netflix clone 
+- Netflix clone by using html,css and javascript
+
+![video](./screen_recording.gif)
